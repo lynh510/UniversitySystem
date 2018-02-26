f5688e5acf704843d3303a36eb7a5afc0d525eea
Duc\Ducc <ducpagc00970@fpt.edu.vn>
2018-02-26 18:52:57 +0700

    add comments inside code

--- src/main/java/com/system/controllers/StudentController.java
+++ src/main/java/com/system/controllers/StudentController.java
@@ -7,11 +7,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+	
+	//First run the java file in com.system the parse this into browser
+	//http://localhost:8080/student/registration
 	@RequestMapping("/registration")
 	public ModelAndView registration() {
 		return new ModelAndView("student_registration_form");
 	}
-	
+	//First run the java file in com.system the parse this into browser
+	//http://localhost:8080/student/login
 	@RequestMapping("/login")
 	public ModelAndView login() {
 		return new ModelAndView("student_login");
